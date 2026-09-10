a742b8de56881bbb81c6f1b4034235bfb5074684
Atul Golchha <81626527+atul94063@users.noreply.github.com>
2026-09-10 22:45:15 +0530

    Refactor transactions table creation SQL script

--- Orra-databases/orra_database/migrations/V4_create_transactions_table.sql
+++ Orra-databases/orra_database/migrations/V4_create_transactions_table.sql
@@ -1,11 +1,14 @@
 CREATE TABLE transactions (
-  transaction_id bigint NOT NULL DEFAULT nextval('transactions_transaction_id_seq'::regclass),
-  booking_id bigint,
-  amount double precision NOT NULL,
-  type character varying NOT NULL,
-  status character varying NOT NULL,
-  payment_gateway_ref character varying,
-  created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT transactions_pkey PRIMARY KEY (transaction_id),
-  CONSTRAINT transactions_booking_id_fkey FOREIGN KEY (booking_id) REFERENCES public.bookings(booking_id)
+    transaction_id BIGSERIAL PRIMARY KEY,
+    booking_id BIGINT,
+
+    amount NUMERIC(10,2) NOT NULL,
+    type VARCHAR(20) NOT NULL,
+    status VARCHAR(20) NOT NULL,
+
+    payment_gateway_ref VARCHAR(255),
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (booking_id) REFERENCES bookings(booking_id)
 );
