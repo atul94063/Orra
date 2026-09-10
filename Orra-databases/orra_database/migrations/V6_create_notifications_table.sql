8f3f76202a711bc451fd128a31254fc42f00401a
Atul Golchha <81626527+atul94063@users.noreply.github.com>
2026-09-10 22:54:44 +0530

    Update V6_create_notifications_table.sql

--- Orra-databases/orra_database/migrations/V6_create_notifications_table.sql
+++ Orra-databases/orra_database/migrations/V6_create_notifications_table.sql
@@ -1,13 +1,15 @@
 CREATE TABLE notifications (
-  id bigint NOT NULL DEFAULT nextval('notifications_id_seq'::regclass),
-  user_id bigint NOT NULL,
-  booking_id bigint,
-  message text NOT NULL,
-  type character varying NOT NULL,
-  is_read boolean NOT NULL DEFAULT false,
-  created_at timestamp without time zone NOT NULL DEFAULT now(),
-  CONSTRAINT notifications_pkey PRIMARY KEY (id),
-  CONSTRAINT notifications_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(user_id),
-  CONSTRAINT notifications_booking_id_fkey FOREIGN KEY (booking_id) REFERENCES public.bookings(booking_id)
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    booking_id BIGINT,
+
+    message TEXT NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    is_read BOOLEAN DEFAULT FALSE,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (booking_id) REFERENCES bookings(booking_id)
 );
 -- CREATE INDEX idx_notifications_user_id ON notifications(user_id);
