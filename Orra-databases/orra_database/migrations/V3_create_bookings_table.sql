7fb683177149ac5ec6bb0b7c5e7adc85e702a574
Atul Golchha <81626527+atul94063@users.noreply.github.com>
2026-09-10 22:43:50 +0530

    Refactor bookings table creation SQL script

--- Orra-databases/orra_database/migrations/V3_create_bookings_table.sql
+++ Orra-databases/orra_database/migrations/V3_create_bookings_table.sql
@@ -1,18 +1,23 @@
 CREATE TABLE bookings (
-  booking_id bigint NOT NULL DEFAULT nextval('bookings_booking_id_seq'::regclass),
-  listing_id bigint,
-  renter_id bigint,
-  start_datetime date NOT NULL,
-  end_datetime date NOT NULL,
-  total_price numeric,
-  deposit_amount numeric,
-  status character varying NOT NULL DEFAULT 'pending'::booking_status_enum,
-  created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-  accepted_at timestamp with time zone,
-  completed_at timestamp with time zone,
-  paid_at timestamp with time zone,
-  shipped_at timestamp with time zone,
-  CONSTRAINT bookings_pkey PRIMARY KEY (booking_id),
-  CONSTRAINT bookings_listing_id_fkey FOREIGN KEY (listing_id) REFERENCES public.listings(product_id),
-  CONSTRAINT bookings_renter_id_fkey FOREIGN KEY (renter_id) REFERENCES public.users(user_id)
+    booking_id BIGSERIAL PRIMARY KEY,
+
+    listing_id BIGINT,
+    renter_id BIGINT,
+
+    start_datetime DATE NOT NULL,
+    end_datetime DATE NOT NULL,
+
+    total_price NUMERIC(10,2),
+    deposit_amount NUMERIC(10,2),
+
+    status VARCHAR(20) DEFAULT 'PENDING',
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    accepted_at TIMESTAMP,
+    paid_at TIMESTAMP,
+    shipped_at TIMESTAMP,
+    completed_at TIMESTAMP,
+
+    FOREIGN KEY (listing_id) REFERENCES listings(product_id),
+    FOREIGN KEY (renter_id) REFERENCES users(user_id)
 );
