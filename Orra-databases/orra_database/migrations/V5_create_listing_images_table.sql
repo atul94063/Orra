6453b9267892dbbe41e7fee076e35b94cd7c0991
Atul Golchha <81626527+atul94063@users.noreply.github.com>
2026-09-10 22:46:41 +0530

    Update V5_create_listing_images_table.sql

--- Orra-databases/orra_database/migrations/V5_create_listing_images_table.sql
+++ Orra-databases/orra_database/migrations/V5_create_listing_images_table.sql
@@ -1,11 +1,13 @@
 CREATE TABLE listing_images (
-  image_id bigint NOT NULL DEFAULT nextval('listing_images_image_id_seq'::regclass),
-  listing_id bigint NOT NULL,
-  image_url text NOT NULL,
-  is_cover boolean NOT NULL DEFAULT false,
-  display_order integer NOT NULL DEFAULT 0,
-  created_at timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  image_data text,
-  CONSTRAINT listing_images_pkey PRIMARY KEY (image_id),
-  CONSTRAINT listing_images_listing_id_fkey FOREIGN KEY (listing_id) REFERENCES public.listings(product_id)
+    image_id BIGSERIAL PRIMARY KEY,
+    listing_id BIGINT NOT NULL,
+
+    image_url TEXT NOT NULL,
+    is_cover BOOLEAN DEFAULT FALSE,
+    display_order INT DEFAULT 0,
+    image_data TEXT,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (listing_id) REFERENCES listings(product_id)
 );
