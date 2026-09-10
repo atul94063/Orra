9ec368ecc3f607bdf4504f498e61ebe969de8fc5
Atul Golchha <81626527+atul94063@users.noreply.github.com>
2026-09-11 00:08:43 +0530

    Refactor user_roles table creation syntax

--- Orra-databases/orra_database/migrations/V8_create_user_roles_table.sql
+++ Orra-databases/orra_database/migrations/V8_create_user_roles_table.sql
@@ -1,6 +1,8 @@
 CREATE TABLE user_roles (
-  user_id bigint NOT NULL,
-  role character varying NOT NULL,
-  CONSTRAINT user_roles_pkey PRIMARY KEY (user_id, role),
-  CONSTRAINT fk_user_roles_user FOREIGN KEY (user_id) REFERENCES .users(user_id)
+    user_id BIGINT,
+    role VARCHAR(20),
+
+    PRIMARY KEY (user_id, role),
+
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
